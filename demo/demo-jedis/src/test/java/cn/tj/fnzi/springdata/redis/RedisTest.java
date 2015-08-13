1ca5f7196423b7957b505dfd5573b37173379aa8
fnzi <787488802@qq.com>
2015-08-13 14:29:48 +0800

    quartz-cluster

--- demo/demo-jedis/src/test/java/cn/tj/fnzi/springdata/redis/RedisTest.java
+++ demo/demo-jedis/src/test/java/cn/tj/fnzi/springdata/redis/RedisTest.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,29 +24,49 @@ public class RedisTest {
     @Autowired
     protected RedisTemplate<Serializable, Serializable> redisTemplate;
 
+    @Autowired
+    private RedisSerializer<Object> redisSerializer;
+
     @Test
     public void test001() {
-        
+
         Map<String, String> map = new HashMap<String, String>();
-        map.put("id","1");
+        map.put("id", "1");
         map.put("username", "fnzi");
         map.put("password", "123");
-        
+
         Map<String, String> map1 = new HashMap<String, String>();
-        map1.put("id","2");
+        map1.put("id", "2");
         map1.put("username", "fnzi2");
         map1.put("password", "123");
-        
+
         final List<Map<String, String>> list = new ArrayList<Map<String, String>>();
         list.add(map);
         list.add(map1);
-        
-        
+
         redisTemplate.execute(new RedisCallback<Object>() {
             @Override
-            public Object doInRedis(RedisConnection connection) throws DataAccessException {
-                connection.set(redisTemplate.getStringSerializer().serialize("123"),
-                               redisTemplate.getStringSerializer().serialize(""));
+            public Object doInRedis(RedisConnection connection)
+                    throws DataAccessException {
+                connection.set(
+                        redisTemplate.getStringSerializer().serialize("1232"),
+                        redisSerializer.serialize(list));
+                return null;
+            }
+        });
+
+        redisTemplate.execute(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection connection)
+                    throws DataAccessException {
+                byte[] bytes = connection.get(
+                        redisTemplate.getStringSerializer().serialize("1231"));
+
+                List<Map<String, String>> vo = (List<Map<String, String>>) redisSerializer
+                        .deserialize(bytes);
+                for (Map<String, String> m : vo) {
+                    System.out.println(m.get("id"));
+                }
                 return null;
             }
         });
